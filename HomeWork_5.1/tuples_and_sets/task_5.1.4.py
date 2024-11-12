ca4f4a6286af1aa39f389525bc638f17580473d1
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-11-12 18:34:30 +0300

    Сделал четвёртую задачу

--- HomeWork_5.1/tuples_and_sets/task_5.1.4.py
+++ HomeWork_5.1/tuples_and_sets/task_5.1.4.py
@@ -4,3 +4,16 @@ cinema_genres_set = set(cinema_genres)
 cinema_genres_set.add('Исторический')
 cinema_genres_set.add('Документальный')
 print(cinema_genres_set)
+print(type(cinema_genres_set))
+
+cinema_genres_set.remove('пеплум')
+cinema_genres_set.remove('экшн')
+delete_genres = cinema_genres_set.pop()
+
+print(f'Удалили жанр: {delete_genres}')
+print(cinema_genres_set)
+
+
+cinema_genres = list(cinema_genres_set)
+print(cinema_genres)
+print(type(cinema_genres))
