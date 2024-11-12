a5153ac8d64aa9d9a5f21a39c0ce5a54753e6ae8
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-11-12 18:28:37 +0300

    Чуть переделал

---
+++ HomeWork_5.1/tuples_and_sets/task_5.1.4.py
@@ -0,0 +1,6 @@
+cinema_genres = ["комедия", "экшн", "пеплум", "триллер", "комедия", "пеплум"]
+
+cinema_genres_set = set(cinema_genres)
+cinema_genres_set.add('Исторический')
+cinema_genres_set.add('Документальный')
+print(cinema_genres_set)
