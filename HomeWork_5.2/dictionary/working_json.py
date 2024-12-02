eb76e24c8b735f4db3755008246180bceb8a9dbb
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2024-12-02 15:47:34 +0300

    Доделал программу с функциями и разбил на файлы

--- HomeWork_5.2/dictionary/working_json.py
+++ HomeWork_5.2/dictionary/working_json.py
@@ -1,13 +1,14 @@
 import json
 
 def create_json_file(file_path, test_answer):
+    '''Создаёт json файл с ответами пользователя'''
     with open(f'{file_path}.json', 'w', encoding='UTF-8') as file:
         json.dump(test_answer, file, indent=2)
     return file_path
 
 
 def get_unpacking_json() -> list:
-
+    '''Распаковывает json файл'''
     with open('questions.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
